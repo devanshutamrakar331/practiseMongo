9414b4e59528b6f47adef7db3351b290aae92166
Devanshu Tamrakar <officialdevanshu331@gmail.com>
2023-02-19 12:32:22 +0530

    first commit

--- app.js
+++ app.js
@@ -1,27 +1,29 @@
-const express=require("express")
-const app=express()
-require("dotenv").config()
-const mongoose=require("mongoose")
-const studentRouter=require("./routes/student")
-const teacherRouter=require("./routes/teacher")
-
+const express = require("express");
+const app = express();
+require("dotenv").config();
+const mongoose = require("mongoose");
+const studentRouter = require("./routes/student");
+const teacherRouter = require("./routes/teacher");
 
 //middlewares
-app.use(express.json())
+app.use(express.json());
 
-app.use(studentRouter)
-app.use(teacherRouter)
+//user made middlewares
+app.use(studentRouter);
+app.use(teacherRouter);
 
 // mongoose coonection
-mongoose.set('strictQuery',false)
-mongoose.connect("mongodb://localhost/practiseMongo").then(db=>{
-    console.log("DB CONNECTED ")
-}).catch(err=>console.log(err))
-
+mongoose.set("strictQuery", false);
+mongoose
+  .connect("mongodb://localhost/practiseMongo")
+  .then((db) => {
+    console.log("DB CONNECTED ");
+  })
+  .catch((err) => console.log(err));
 
 //server connection
-const port=process.env.PORT
+const port = process.env.PORT;
 
-app.listen(port,()=>{
-    console.log("Server running at port:",port)
-})
+app.listen(port, () => {
+  console.log("Server running at port:", port);
+});
